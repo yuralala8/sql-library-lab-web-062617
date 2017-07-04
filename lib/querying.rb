3e0b07e652497af5e1bc7736ce9dcbdfbef0a137
yuralala8 <yc1435@nyu.edu>
2017-07-03 23:45:48 -0400

    sql library lab

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,17 +1,23 @@
 def select_books_titles_and_years_in_first_series_order_by_year
+  "SELECT books.title, books.year FROM books INNER JOIN series ON books.series_id = series.id WHERE series.id = 1 ORDER BY books.year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
+  "SELECT characters.name, characters.motto FROM characters WHERE length(characters.motto) = (SELECT MAX(LENGTH(characters.motto)) FROM characters)"
 end
 
 def select_value_and_count_of_most_prolific_species
+  "SELECT characters.species, COUNT(characters.species) FROM characters GROUP BY species ORDER BY species DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
+  "SELECT authors.name, subgenres.name FROM authors INNER JOIN subgenres ON authors.id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
+  "SELECT series.title FROM series INNER JOIN characters ON series.id = characters.series_id ORDER BY characters.name DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
+  "SELECT characters.name, COUNT(character_books.book_id) FROM characters INNER JOIN character_books WHERE characters.id = character_books.character_id GROUP BY characters.name ORDER BY COUNT(character_books.book_id) DESC"
 end
